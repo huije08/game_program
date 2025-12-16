d9397e9ddb6c4964bf5524f9ba8de6b2898c5c3f
huije08 <huijegang@gmail.com>
2025-12-16 19:46:09 +0900

    (2025.12.16)

---
+++ program1/Assets/Class5th (User Interface)/Scripts/MianMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Start");
+    }
+
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+
+}
